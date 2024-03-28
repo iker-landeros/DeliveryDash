9d36d7d2dd116413a6b616451575a4e19c18c3bd
diego-deese <diego_dsgg@hotmail.com>
2024-03-27 19:23:19 -0600

    added user and users with most time played endpoints

--- 3-BackEnd/controllers/usuario.controller.js
+++ 3-BackEnd/controllers/usuario.controller.js
@@ -9,6 +9,31 @@ const getUsuarios = (req, res) => {
     })
 }
 
+const getMostPlayedUsuario = (req, res) => {
+    const sql = `SELECT Usuario.fName, Sesion.sesionTime 
+                 FROM Usuario 
+                 INNER JOIN Sesion ON Usuario.userID = Sesion.userID
+                 WHERE Sesion.sesionTime = (SELECT MAX(sesionTime) FROM Sesion AS max)
+                 ORDER BY Sesion.date;`
+
+    pool.query(sql, (err, results, fields) => {
+        if (err) res.json(err)
+        res.json(results)
+    })
+}
+
+const getMostPlayedAll = (req, res) => {
+    const sql = `SELECT Usuario.fName, Sesion.sesionTime 
+                 FROM Usuario INNER JOIN Sesion
+                 ON Usuario.userID = Sesion.userID
+                 ORDER BY Sesion.sesionTime DESC;`
+
+    pool.query(sql, (err, results, fields) => {
+        if (err) res.json(err)
+        res.json(results)
+    })
+}
+
 const insertUsuario = (req, res) => {
     const {mail, fName, lName} = req.body
     const sql = `INSERT INTO Usuario(mail, fName, lName)
@@ -19,4 +44,6 @@ const insertUsuario = (req, res) => {
     })
 }
 
-module.exports = { getUsuarios, insertUsuario }
+
+
+module.exports = { getUsuarios, getMostPlayedUsuario, getMostPlayedAll, insertUsuario }
--- 3-BackEnd/routes/usuario.js
+++ 3-BackEnd/routes/usuario.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { getUsuarios, insertUsuario } = require('../controllers/usuario.controller')
+const { getUsuarios, getMostPlayedUsuario, getMostPlayedAll, insertUsuario } = require('../controllers/usuario.controller')
 
 router.get('/usuario', getUsuarios)
+router.get('/usuario/most', getMostPlayedUsuario)
+router.get('/usuario/most/all', getMostPlayedAll)
 router.post('/usuario', insertUsuario)
 
 module.exports = router
